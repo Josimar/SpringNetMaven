1a5e292f5d857536e3a626ae23df323e961487e3
Josimar Silva <josimar.silva@me.com.br>
2021-08-19 17:21:49 -0300

    Default Profile

--- src/main/java/com/josimas/projectone/services/I18nPortugueseService.java
+++ src/main/java/com/josimas/projectone/services/I18nPortugueseService.java
@@ -3,7 +3,7 @@ package com.josimas.projectone.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PT")
+@Profile({"PT", "default"})
 @Service("i18nService")
 public class I18nPortugueseService implements GreetingService{
     @Override
