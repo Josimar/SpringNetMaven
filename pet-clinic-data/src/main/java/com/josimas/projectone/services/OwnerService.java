0614ebde646f93be7ec8a7aaa11fdec0fb62409e
Josimar Silva <josimar.silva@me.com.br>
2021-08-20 12:42:03 -0300

    Refactoring to common CRUD Interface

--- pet-clinic-data/src/main/java/com/josimas/projectone/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/josimas/projectone/services/OwnerService.java
@@ -2,15 +2,9 @@ package com.josimas.projectone.services;
 
 import com.josimas.projectone.owner.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/josimas/projectone/services/PerService.java
+++ pet-clinic-data/src/main/java/com/josimas/projectone/services/PerService.java
@@ -2,14 +2,6 @@ package com.josimas.projectone.services;
 
 import com.josimas.projectone.owner.Pet;
 
-import java.util.Set;
-
-public interface PerService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PerService extends CrudService<Pet, Long> {
 
 }
