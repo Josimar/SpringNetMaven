c34fb3f435d29c0fcbef2b20d248b23a97703d0c
Josimar Silva <josimar.silva@me.com.br>
2021-08-23 14:08:43 -0300

    Added owners to controller. Closes #15

--- pet-clinic-web/src/main/java/com/josimas/projectone/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/josimas/projectone/bootstrap/DataLoader.java
@@ -1,15 +1,31 @@
 package com.josimas.projectone.bootstrap;
 
+import com.josimas.projectone.model.Role;
+import com.josimas.projectone.model.User;
 import com.josimas.projectone.owner.Owner;
+import com.josimas.projectone.repository.RoleRepository;
+import com.josimas.projectone.repository.UserRepository;
 import com.josimas.projectone.services.OwnerService;
 import com.josimas.projectone.services.VetService;
 import com.josimas.projectone.vet.Vet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     private final OwnerService ownerService;
     private final VetService vetService;
@@ -21,6 +37,20 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        roleRepository.save(new Role("USER"));
+        roleRepository.save(new Role("ADMIN"));
+
+        Role adminRole = roleRepository.findByRole("ADMIN");
+        Role userRole = roleRepository.findByRole("USER");
+
+        User user = new User("josimar@gmail.com", passwordEncoder.encode("mar1976"), "Josimar", "Silva", true, "josimarb");
+        user.setRoles(List.of(adminRole));
+        userRepository.save(user);
+
+        user = new User("josimas@gmail.com", passwordEncoder.encode("mar1976"), "Josimar", "Silva", true, "josimas");
+        user.setRoles(List.of(userRole));
+        userRepository.save(user);
+
         System.out.println(" ");
         System.out.println("********** Auto Load **********");
 
