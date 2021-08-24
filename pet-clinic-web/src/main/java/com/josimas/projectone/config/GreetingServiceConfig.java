39609b960c105528f29aa7f3eeba8187d61a775d
Josimar Silva <josimar.silva@me.com.br>
2021-08-24 14:20:57 -0300

    Spring Boot Profile Application Properties

--- pet-clinic-web/src/main/java/com/josimas/projectone/config/GreetingServiceConfig.java
+++ pet-clinic-web/src/main/java/com/josimas/projectone/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import com.josimas.projectone.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:pet-config.xml")
 @Configuration
 public class GreetingServiceConfig {
