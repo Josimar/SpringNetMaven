c03dbe1bba78e4ad7e18a24f6659d96cb5d00631
Josimar Silva <josimar.silva@me.com.br>
2021-08-24 19:20:27 -0300

    Add PetType Service

---
+++ pet-clinic-data/src/main/java/com/josimas/projectone/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.josimas.projectone.services;
+
+import com.josimas.projectone.owner.PetType;
+
+import java.util.Set;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
