f3b73c1776d3c4d56972084abbb9e5d6ce02e112
Josimar Silva <josimar.silva@me.com.br>
2021-08-26 16:08:22 -0300

    IMplemented JPA Visit Service

--- pet-clinic-data/src/main/java/com/josimas/projectone/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/josimas/projectone/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package com.josimas.projectone.services.map;
 
 import com.josimas.projectone.services.VisitService;
 import com.josimas.projectone.visit.Visit;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
