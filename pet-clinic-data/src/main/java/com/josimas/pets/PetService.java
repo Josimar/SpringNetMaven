63ecf87ba086c1547d0380baa79cdd4cd90ed598
Josimar Silva <josimar.silva@me.com.br>
2021-08-26 13:42:35 -0300

    Create Spring Data JPA Pet

--- pet-clinic-data/src/main/java/com/josimas/pets/PetService.java
+++ pet-clinic-data/src/main/java/com/josimas/pets/PetService.java
@@ -3,7 +3,7 @@ package com.josimas.pets;
 import com.josimas.projectone.owner.Pet;
 import com.josimas.projectone.services.CrudService;
 
-public interface PetService  {
+public interface PetService extends CrudService<Pet, Long>  {
 
     String getPetType();
 
---
+++ pet-clinic-data/src/main/java/com/josimas/projectone/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,51 @@
+package com.josimas.projectone.services.springdatajpa;
+
+import com.josimas.pets.PetService;
+import com.josimas.projectone.owner.Pet;
+import com.josimas.projectone.repositories.PetRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public String getPetType() {
+        return null;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
