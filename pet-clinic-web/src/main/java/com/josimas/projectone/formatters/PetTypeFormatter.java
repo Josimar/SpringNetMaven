81c669b32687202fcc986457db0859009f0cf801
Josimar Silva <josimar.silva@me.com.br>
2021-08-31 15:03:54 -0300

    Pet type formatter

---
+++ pet-clinic-web/src/main/java/com/josimas/projectone/formatters/PetTypeFormatter.java
@@ -0,0 +1,40 @@
+package com.josimas.projectone.formatters;
+
+import com.josimas.projectone.owner.PetType;
+import com.josimas.projectone.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+import static java.awt.SystemColor.text;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes){
+            if (type.getName().equals(text)){
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+}
