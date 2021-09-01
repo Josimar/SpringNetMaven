952ea6a98ec914d8d659fd20ac7336e533b4c296
Josimar Silva <josimar.silva@me.com.br>
2021-09-01 10:54:17 -0300

    Added JSON endPoint

--- pet-clinic-web/src/main/java/com/josimas/projectone/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/josimas/projectone/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.josimas.projectone.controllers;
 
+import java.util.Set;
 import com.josimas.projectone.services.VetService;
+import com.josimas.projectone.vet.Vet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VetController {
@@ -14,11 +18,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @RequestMapping({"vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 
     @RequestMapping({"/oups"})
